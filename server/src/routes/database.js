97afb4fa42a874d0628f9ba2261590c129dd97ac
xtombrock <xtombrock1989@gmail.com>
2024-10-17 23:37:22 +0100

    sae

--- server/src/routes/database.js
+++ server/src/routes/database.js
@@ -17,4 +17,10 @@ router.post('/create-new-database', createDatabaseHelper);
 router.get('/get-database-by-name', getDatabaseByNameHelper);
 router.post('/create-new-table', createTableHelper);
 
+router.post('/create-new-shadow-database', createDatabaseHelper);
+router.delete('/delete-database', createTableHelper);
+router.get('/generate-link', createTableHelper);
+router.delete('/delete-all-tables', createTableHelper);
+router.delete('/clear-all-entries-for-database', createTableHelper);
+
 export default router;
